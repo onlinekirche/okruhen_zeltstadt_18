35dc2e18e5a0fea79780f34bfe4f9ac944961b8c
Joerg Sorge <joergsorge@gmail.com>
2018-08-06 15:03:31 +0200

    initial

---
+++ index.php
@@ -0,0 +1,379 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="de">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="OKruhen ist ein Projekt der OnlineKirche in der Evangelischen Kirche Mitteldeutschlands">
+    <meta name="keywords" content="Online, Kirche, Evangelisch, Ev, Evang, Mitteldeutschland, Christus, Jesus, Protestant, protestantisch, EKM, EKMD, Glauben, OKglauben, lutherisch">
+    <meta name="author" content="OnlineKirche, Joerg Sorge">
+    <!-- <meta http-equiv="cache-control" content="no-cache" />-->
+
+    <title>OnlineKirche Ruhen</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/one-page-wonder.css" rel="stylesheet">
+    <link href="css/one-page-wonder-custom.css" rel="stylesheet">
+
+    <!-- Matomo -->
+    <script type="text/javascript">
+        var _paq = _paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+            var u="//onlinekirche.net/piwik/";
+            _paq.push(['setTrackerUrl', u+'piwik.php']);
+            _paq.push(['setSiteId', '2']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+        })();
+    </script>
+    <!-- End Matomo Code -->
+
+  </head>
+
+  <body>
+      <!-- Bootstrap core JavaScript -->
+      <!-- Must load before commentics -->
+      <script src="vendor/jquery/jquery.min.js"></script>
+      <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Home</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#impuls1">Impuls 1</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#impuls2">Impuls 2</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#impuls3">Impuls 3</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#fragen">Fragen</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#stream">Stream</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#comment">Kommentieren</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <header class="masthead text-center text-white">
+      <div class="masthead-content">
+        <div class="container">
+            <h1 class="masthead-heading"> <br>  </h1>
+            <h1 class="masthead-heading"> <br>  </h1>
+            <h1 class="masthead-heading mb-0">#OKruhen</h1>
+            <h2 class="masthead-subheading mb-0">OnlineKirche</h2>
+            <h1 class="masthead-heading"> <br>  </h1>
+            <h1 class="masthead-heading"> <br>  </h1>
+            <h1 class="masthead-heading"> </h1>
+        </div>
+      </div>
+      <div class="bg-circle-1 bg-circle"></div>
+      <div class="bg-circle-2 bg-circle"></div>
+      <div class="bg-circle-3 bg-circle"></div>
+      <div class="bg-circle-4 bg-circle"></div>
+    </header>
+
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="p-5">
+                <h2 class="text-center">"Es ist Zeit, auszuruhen"</h2>
+              <p class="text-center"><br>Das war das Thema bei einer Bibelarbeit auf der <a href="http://www.die-zeltstadt.de/" target="_blank">Zeltstadt in Neufrankenroda</a> im Sommer 2018.</p>
+              <p class="text-center">Am Montag, 30.07.2018 sprach Schwester Elise über einen alttestamentlichen <a href="https://www.bibleserver.com/text/LUT/1.Mose1" target="_blank">Bibeltext</a>. Um 11 Uhr folgte das Nachgespräch.</p>
+              <p class="text-center">Auf dieser Website übertrugen wir die beiden Veranstaltungen im Livestream.</p>
+              <p class="text-center">Ihr könnt hier direkt kommentieren, auf unserer <img class="img-fluid" src="img/social_facebook_box_blue.32.png" alt="Logo facebook">
+                  <a href="https://www.facebook.com/Onlinekirche-888512347994626/" target="_blank">facebook-Seite</a>,
+                  auf <img class="img-fluid" src="img/IG_Glyph_Fill.png" alt="Logo Instagram">
+                  <a href="https://www.instagram.com/onlinekirche/" target="_blank">Instagram</a>
+                  oder in der <a href="https://play.google.com/store/apps/details?id=com.communi.zeltstadtthueringen" target="_blank">Zeltsdadt-App</a>.<br>
+                  Bitte benutzt immer das Hashtag #OKruhen</p>
+            </div>
+          </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="p-5">
+              <img class="img-fluid" src="img/Luftbild_1.png" alt="Luftbild von der Zeltstadt c. Zeltstadt">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="impuls1">
+      <div class="container bg-alt-3">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="p-5">
+              <h2 class="display-4 text-center fg-alt-3">#OKruhen</br>Was hilft mir zur Ruhe zu kommen?</h2>
+              <p class="text-center fg-alt-3">    Oft sind wir hektisch unterwegs. Anforderungen prasseln auf uns ein. Zur Ruhe kommen fällt uns schwer. Es braucht eine bewusste Entscheidung, nicht noch mehr zu tun, sondern abzuschalten und Ruhe zu suchen. Was sind deine persönlichen Ruhestrategien, was hilft dir zur Ruhe zu kommen?</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="p-5">
+                <div class="media">
+                    <img class="mr-3" src="img/myavatar_male.100.png" alt="Avatar männlich">
+                    <div class="media-body">
+                        <h4>Karsten</h4>
+                            <p>Ab und zu mal in die Sauna gehen (auch im Sommer) und allen Lärm des Alltags für vier Stunden hinter mir lassen. Da finde ich Ruhe!</p>
+                    </div>
+                </div>
+            </div>
+            <div class="p-5">
+                <div class="media">
+                    <img class="mr-3" src="img/myavatar_female.100.png" alt="Avatar weiblich">
+                    <div class="media-body">
+                        <h4>Christina</h4>
+                            <p>Klingel abstellen, Musik auf die Ohren und lesen.<br>
+                                Oder am See in einer ruhigen Ecke auf den Rücken legen und den Wolken beim Tanz mit den Blättern zusehen.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="p-5">
+                <div class="media">
+                    <img class="mr-3" src="img/myavatar_female.100.png" alt="Avatar weiblich">
+                    <div class="media-body">
+                        <h4>Martha</h4>
+                            <p>An einen schönen Ort gehen: am Besten in die Natur, einen See, in den Wald. Oder in mein Bett :)</p>
+                    </div>
+                </div>
+            </div>
+          </div>
+      </div>
+      </div>
+    </section>
+
+    <section id="impuls2">
+      <div class="container bg-alt-3">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="p-5">
+              <h2 class="display-4 text-center fg-alt-3">#OKruhen</br>Kann Gott mir Ruhe verschaffen?</h2>
+              <p class="text-center fg-alt-3">    Wenn ich selber nicht zur Ruhe komme, wie kann dann jemand von außen mir Ruhe verschaffen? <br>Viele Menschen erzählen, dass sie bei Gott zur Ruhe gefunden haben. Gebet, Bibel lesen, Meditation, Stille, ... <br>Wie kommst du "vor Gott zur Ruhe"?</p>
+            </div>
+          </div>
+      </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="p-5">
+                <div class="media">
+                    <img class="mr-3" src="img/myavatar_male.100.png" alt="Avatar männlich">
+                    <div class="media-body">
+                        <h4>Karsten</h4>
+                            <p>Ich kann mich mit allen Sorgen an Gott wenden. Ich weiß, dass er größer ist als meine Probleme, dadurch komme ich zur Ruhe und kann Dinge ganz entspannt auf mich zukommen lassen...</p>
+                    </div>
+                </div>
+            </div>
+            <div class="p-5">
+                <div class="media">
+                    <img class="mr-3" src="img/myavatar_male.100.png" alt="Avatar männlich">
+                    <div class="media-body">
+                        <h4>Ramón</h4>
+                            <p>Das gelingt mal besser und mal schlechter. Nicht immer bin ich offen für Erfahrungen mit Gott. Manchmal frage ich: was heißt das eigentlich? Aber wenn es gelingt, ist es wunderbar und entlastend und schenkt mir Freiheit, Gelassenheit und Ruhe, weil ich meine Grenzen erkenne</p>
+                    </div>
+                </div>
+            </div>
+          </div>
+          </div>
+      </div>
+    </section>
+
+    <section id="impuls3">
+      <div class="container bg-alt-3">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="p-5">
+              <h2 class="display-4 text-center fg-alt-3">#OKruhen</br>„Und siehe es war NICHT gut“. Wie kann ich dennoch zur Ruhe kommen?</h2>
+              <p class="text-center fg-alt-3">
+              Wenn ein Werk vollbracht ist, eine Aufgabe erledigt und das Glücksgefühl einsetzt, dann kann man gut entspannen. Aber was, wenn eine Aufgabe offen bleibt, wenn ein Projekt gescheitert ist oder mich etwas  bedrückt?<br>
+              Gerade da bräuchte man Ruhe so dringend. Was sagst du, wenn du auf deine Arbeit, dein Leben schaust?
+              </p>
+            </div>
+          </div>
+      </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="p-5">
+                <div class="media">
+                    <img class="mr-3" src="img/myavatar_female.100.png" alt="Avatar weiblich">
+                    <div class="media-body">
+                        <h4>Karola</h4>
+                            <p>Wie komm ich zur Ruhe? Ich gehe raus in die Natur, jeden Tag nehm ich mir die Zeit, Bibel im Rucksack und dann losrennen. Meine Zeit allein mit Gott, das ist mega im wahrsten Sinne geil!!!! Denn nur mit IHM ist es richtig cool.</br>
+                             Leider gelingt es mir immer noch nicht, danach, im Alltag zurück kontinuierlich dranzubleiben. Aber, “Gott sei Dank“, mein“Papa“ ist gnädig mit mir, bloß ich selber oft nicht mit mir....
+                            <br>
+                            Heute gerade eben lese ich Jeremia, Gottsdienstpredigt. Hab nochmal nachgelesen und mich gefragt, wo stehe ich... Das ist“ zur Ruhe kommen“ für mich....
+                            </p>
+                    </div>
+                </div>
+            </div>
+
+          </div>
+          </div>
+      </div>
+    </section>
+
+    <section id="fragen">
+      <div class="container bg-alt-3">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="p-5">
+              <h2 class="display-4 text-center fg-alt-3">Zum Bibeltext könnt ihr Fragen stellen.</h2>
+              <p class="text-center fg-alt-3">
+              Der <a href="https://www.bibleserver.com/text/LUT/1.Mose1" target="_blank">Text</a> steht am Anfang der Bibel.
+              Schwester Elise von der Christusbruderschaft Selbitz wird darüber sprechen.
+              Ob beim Livestream oder beim Selbststudium, wenn euch Fragen kommen, dann teilt sie mit uns!<br>
+              Z.B. hier unten in den Kommentaren, auf facebook, Instagram oder Twitter mit dem Hashtag #OKglauben.<br>
+              Im Nachgespräch zur Bibelarbeit greifen wir eure Fragen auf.
+              </p>
+            </div>
+          </div>
+      </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="p-5">
+                <div class="media">
+                    <img class="mr-3" src="img/myavatar_female.100.png" alt="Avatar weiblich">
+                    <div class="media-body">
+                        <h4>Britta</h4>
+                            <p>Gott hat uns eine wunderbare und perfekte Erde geschenkt. Er hat uns und mir Freiheit geschenkt. Jeder kann entscheiden, wie er diesen und jeden neuen Tag mit seinen 24 Stunden möchte (eingeschlossen die eigenen Verpflichtungen wie: Arbeit, Freizeit, Familie- = auch eigene Entscheidungen!)
+Ich schaue morgens innerlich dankbar auf diesen neuen Tag und plane mir eine Zeit, nur mit Gott, ein und freue mich darauf.<br>
+Und versuche, einen etwa gleichen Rhythmus für jeden Tag zu finden. Das stärkt mich sehr.<br>
+Ich darf flexibel sein in der Zeit Einteilung. Es muss nicht jeden Tag um 5 h sein oder ähnlich. Wichtig ist, überhaupt ZEIT mit meinem Vater im Himmel zu verbringen!
+                             </p>
+                    </div>
+                </div>
+            </div>
+            <div class="p-5">
+                <div class="media">
+                    <img class="mr-3" src="img/myavatar_male.100.png" alt="Avatar männlich">
+                    <div class="media-body">
+                        <h4>Ramón</h4>
+                            <p>"Das Land des Siebten Tages" ... "Es ist Zeit zum Überlassen" ... Was heißt das konkret für mein Leben?</p>
+                    </div>
+                </div>
+            </div>
+          </div>
+          </div>
+      </div>
+    </section>
+
+    <section id="stream">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="p-5">
+              <div class="embed-responsive embed-responsive-16by9">
+                  <iframe width="560" height="315" src="https://www.youtube.com/embed/mHBCu0jcEwA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+              </div>
+              </div>
+              <div class="p-5">
+              <div class="embed-responsive embed-responsive-16by9">
+                  <iframe width="560" height="315" src="https://www.youtube.com/embed/gNXj82TU9Bc" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+              </div>
+              </div>
+              <div class="p-5">
+              <div class="embed-responsive embed-responsive-16by9">
+                  <iframe width="560" height="315" src="https://www.youtube.com/embed/2_tyWaSnMCY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+              </div>
+            </div>
+          </div>
+      </div>
+      </div>
+    </section>
+
+    <section id="comment">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="p-5">
+              <p class="text-center"><?php
+              $cmtx_identifier = '2';
+              $cmtx_reference  = 'Page okruhen';
+              $cmtx_folder     = '/inc-cm-9b9df53e6e895e2cea/';
+              require($_SERVER['DOCUMENT_ROOT'] . $cmtx_folder . 'frontend/index.php');
+              ?></p>
+            </div>
+          </div>
+       </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="p-5">
+              <p class="text-center">"#OKruhen" ist ein Projekt der <a href="https://onlinekirche.ekmd.de" target="_blank">OnlineKirche</a>
+              (<a href="https://www.erprobungsraeume-ekm.de/" target="_blank">Erprobungsraum</a> der <a href="https://ekmd.de" target="_blank">Evangelischen Kirche in Mitteldeutschland</a>)</p>
+              <p class="text-center"><a href="https://onlinekirche.ekmd.de/service/impressum/" target="_blank">Impressum</a><br>
+              <a href="https://onlinekirche.ekmd.de/service/datenschutz/" target="_blank">Datenschutz</a><br>
+              <a href="https://onlinekirche.ekmd.de/mitmachen/newsletter/" target="_blank">Newsletter bestellen</a>
+            </div>
+          </div>
+       </div>
+      </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="py-5 bg-black">
+      <div class="container">
+        <p class="m-0 text-center text-white small"><a href="https://github.com/BlackrockDigital/startbootstrap-one-page-wonder" target= "_blank">Credits to BlackrockDigital</a></p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+  </body>
+</html>
